8ca7f869039fe3f3aa1474f2a11691a36901202e
Sudeep <sudeep.rohatgi@gmail.com>
2012-06-26 02:43:55 -0400

    Added Bluetooth 2.1

--- src/com/backyardbrains/RoboroachConfigActivity.java
+++ src/com/backyardbrains/RoboroachConfigActivity.java
@@ -3,8 +3,13 @@ package com.backyardbrains;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.Toast;
 
 public class RoboroachConfigActivity extends Activity implements OnClickListener{
 
@@ -14,8 +19,49 @@ public class RoboroachConfigActivity extends Activity implements OnClickListener
 		setContentView(R.layout.roboroach_configuration);	
 		View helpButton = findViewById(R.id.rr_help_button);
 		helpButton.setOnClickListener(this);
+		View cancelButton = findViewById(R.id.rr_cancel_button);
+		cancelButton.setOnClickListener(this);
+		View saveButton = findViewById(R.id.rr_save_button);
+		saveButton.setOnClickListener(this);
 	}
+	
+	boolean ButtonState = false; 
+	public void onCheckboxClicked(View v) {
+	    // Perform action on clicks, depending on whether it's now checked
 
+		final CheckBox pulseButton = (CheckBox)findViewById(R.id.rr_pulse_button);
+		final CheckBox randomButton = (CheckBox)findViewById(R.id.rr_random_button);
+		
+	    if (pulseButton.isChecked() && ButtonState == false) {
+		    	randomButton.setChecked(false);
+		    	ButtonState = true;
+	    } else if (randomButton.isChecked() && ButtonState == true) {
+	    		pulseButton.setChecked(false);
+	    		ButtonState = false;
+	    }
+	}
+	
+	public void onSave (View v) {
+		if (ButtonState == true) { //Pulse Mode
+			final EditText StimulationAmpField = (EditText) findViewById(R.id.rr_sti_amp_input);  
+			double StimulationAmp = Double.parseDouble(StimulationAmpField.getText().toString());
+			
+			final EditText PulseWidthField = (EditText) findViewById(R.id.rr_pulse_width_input);  
+			double PulseWidth = Double.parseDouble(PulseWidthField.getText().toString()); 
+			
+			final EditText PulseFreqField = (EditText) findViewById(R.id.rr_pulse_freq_input);  
+			double PulseFreq = Double.parseDouble(PulseFreqField.getText().toString()); 
+			
+			final EditText PulseCountField = (EditText) findViewById(R.id.rr_pulse_count_input);  
+			double PulseCount = Double.parseDouble(PulseCountField.getText().toString()); 
+			
+		}
+		else { //Random Mode
+			final EditText StimulationTimeField = (EditText) findViewById(R.id.rr_sti_time_input);  
+			double StimulationTime = Double.parseDouble(StimulationTimeField.getText().toString()); 
+		}
+	}
+	
 	public void onClick(View b) {
 		// TODO Auto-generated method stub
 		switch (b.getId()) {
@@ -32,6 +78,8 @@ public class RoboroachConfigActivity extends Activity implements OnClickListener
 			startActivity(save);
 			break;
 		}
+		
+	
 
 	}
 }
